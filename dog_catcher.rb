bed2824f014e90b292a045d5657589110df6747b
Greg Borenstein <greg@mfdz.com>
2008-12-29 22:44:33 -0800

    moved executable part of code into its own file in a bin/

--- dog_catcher.rb
+++ dog_catcher.rb
@@ -38,23 +38,3 @@ class DogCatcher
   
 end
 
-d = DogCatcher.new
-
-puts "Weclome to the Dog Catcher."
-puts "You currently have #{d.running_mongrels.length} running mongrels."
-
-if d.running_mongrels.length > 0
-  
-  d.running_mongrels.each_with_index do |m,i|
-    puts "#{i+1}. Port: #{DogCatcher.guess_port(m)}"
-  end
-  puts "Enter a number to cleanly shut down that mongrel (or hit return for all): "
-  choice = STDIN.gets.chomp
-  
-  case choice.to_i
-  when 0
-    d.stop_all_mongrels!
-  else
-    DogCatcher.stop_mongrel! d.running_mongrels[choice.to_i - 1]
-  end
-end
